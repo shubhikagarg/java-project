bbb9ad69fe6c390ccc5cfe612495e72bcea92ca3
deepakbedase123 <50132499+deepakbedase123@users.noreply.github.com>
2019-11-29 11:44:31 +0530

    Update TestD2B.java

--- src/test/java/com/app/DecimalToBinary/TestD2B.java
+++ src/test/java/com/app/DecimalToBinary/TestD2B.java
@@ -13,8 +13,15 @@ public class TestD2B
 	public void testpositive()
 	{
 		assertEquals(101, m.D2B(5));
-		String 	 browserName = System.getProperty("browserName");
-		System.out.println(browserName);
+		String 	 instanceID = System.getProperty("instanceID");
+	System.out.println("Instance Id is -   "+instanceID);
+	String uname = System.getProperty("uname");
+	System.out.println(uname);
+	 String[] arrOfStr = uname.split("/"); 
+	
+     for (String a : arrOfStr) 
+         System.out.println("Seprate  is "+a); 
+    
 		
 	}
 	@Test
